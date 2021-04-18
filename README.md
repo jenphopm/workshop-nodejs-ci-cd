41e22c24cf30e22f5e8b847ed45fe0868df1b3cb
up1 <somkiat.p@gmail.com>
2021-04-18 23:49:22 +0700

    First demo

---
+++ README.md
@@ -0,0 +1,23 @@
+# Workshop with [NodeJS](https://nodejs.org/en/)
+
+List of packages
+* express
+* jest
+* jest-junit
+* supertest
+
+## 1. Start node server
+```
+$npm start
+```
+
+Access to api `http://localhost:3000`
+
+## 2. API Testing with [Jest](https://jestjs.io/)
+```
+$npm test
+```
+
+## 3. Deploy to target server
+```
+```
---
+++ __tests__/api_test.js
@@ -0,0 +1,12 @@
+var request = require("supertest");
+var app = require("../src/server.js");
+
+describe("GET /", function () {
+  it("respond with hello world", async function (done) {
+    const response = await request(app).get("/");
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Hello World");
+    done();
+  });
+});
